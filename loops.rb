bb105bd5545a78a4d40cbdd554e0cff4df7c6cf4
JackMarx <seriousfools@gmail.com>
2020-01-08 09:35:24 -0600

    input options

---
+++ loops.rb
@@ -0,0 +1,12 @@
+# cities = ["Jackson", "Southhaven", "Oxford", "Vicksburg", "Booneville", "nowhere"]
+
+# cities.each do |element|
+#   puts "#{ element }, Mississippi"
+# end
+
+number = 0
+
+until number > 4
+  puts "Bats in the belfry"
+  number += 1
+end
--- staff/employee.rb
+++ staff/employee.rb
@@ -13,31 +13,14 @@
 
 
 class Employee
-  def initialize(input_first_name, input_last_name, input_salary, input_active)
-    @first_name = input_first_name
-    @last_name = input_last_name
-    @salary = input_salary
-    @active = input_active
-  end
+  attr_reader :first_name, :last_name, :salary, :active
+  attr_writer :first_name, :last_name, :active
 
-  def first_name
-    @first_name
-  end
-
-  def last_name
-    @last_name
-  end
-
-  def salary
-    @salary
-  end
-
-  def active
-    @active
-  end
-
-  def first_name=(new_first_name)
-    @first_name = new_first_name
+  def initialize(input_options)
+    @first_name = input_options[:first_name]
+    @last_name = input_options[:last_name]
+    @salary = input_options[:salary]
+    @active = input_options[:active]
   end
 
   def print_info
@@ -49,8 +32,19 @@ class Employee
   end
 end
 
-employee_1 = Employee.new("Han", "Solo", 70000, true)
-employee_2 = Employee.new("Lando", "Calrissian", 80000, true)
+employee_1 = Employee.new(
+                          first_name: "Han", 
+                          last_name: "Solo", 
+                          salary: 70000, 
+                          active: true
+                          )
+
+employee_2 = Employee.new(
+                          first_name: "Lando", 
+                          last_name: "Calrissian", 
+                          salary: 80000, 
+                          active: true
+                          )
 
 employee_1.print_info
 employee_2.print_info
