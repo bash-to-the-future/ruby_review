29193b903bc99e9a928b40b273c4fe9ab602cc8b
JackMarx <seriousfools@gmail.com>
2020-01-08 11:16:21 -0600

    super keyword

--- staff/employee.rb
+++ staff/employee.rb
@@ -1,17 +1,3 @@
-
-# employee_1 = ["Han", "Solo", 70000, true]
-# employee_2 = ["Lando", "Calrissian", 80000, true]
-
-# puts employee_1[0] + " " + employee_1[1] + " makes " + employee_1[2].to_s + " a year."
-# puts "#{ employee_2[0] } #{ employee_2[1] } makes #{ employee_2[2] } a year."
-
-# employee_1 = {first_name: "Han", last_name: "Solo", salary: 70000, active: true}
-# employee_2 = {:first_name => "Lando", :last_name => "Calrissian", :salary => 80000, "active" => true}
-
-# puts "#{ employee_1[:first_name] } #{ employee_1[:last_name] } makes #{ employee_1[:salary] } a year."
-# puts "#{ employee_2[:first_name] } #{ employee_2[:last_name] } makes #{ employee_2[:salary] } a year."
-
-
 class Employee
   attr_reader :first_name, :last_name, :salary, :active
   attr_writer :first_name, :last_name, :active
@@ -32,6 +18,21 @@ class Employee
   end
 end
 
+class Manager < Employee
+  attr_reader :employees
+  
+  def initialize(input_options)
+    super(input_options)
+    @employee = input_options[:employees]
+  end
+
+  def send_report
+    puts "Sending Email..."
+    # code to send Email
+    puts "Email sent."
+  end
+end
+
 employee_1 = Employee.new(
                           first_name: "Han", 
                           last_name: "Solo", 
@@ -46,14 +47,15 @@ employee_2 = Employee.new(
                           active: true
                           )
 
-employee_1.print_info
-employee_2.print_info
-employee_1.give_annual_raise
-employee_1.print_info
+manager = Manager.new(
+                      first_name: "Leia",
+                      last_name: "Organa",
+                      salary: 100000,
+                      active: true,
+                      employees: [employee_1, employee_2]
+                      )
 
-p employee_2.first_name
-employee_2.first_name = "Bob"
-p employee_2.first_name
+p manager
 
 
 
